a12bfe17d8bcf41a2c2f81d9f1398ff1077e82c1
derui <derutakayu@gmail.com>
2014-11-30 10:45:58 +0900

    追加部分のViewにイベントを定義

--- assets/js/views/todo-appender.js
+++ assets/js/views/todo-appender.js
@@ -7,8 +7,36 @@ module.exports = Backboen.View.extend({
 
   template: require('tmpl/appender.html'),
 
+  initialize: function() {
+    this.refreshTodo();
+  },
+
   events: {
+    'change .todo__append__date' : 'onDateChange',
+    'change .todo__append__content' : 'onContentChange',
     'click .todo__append__submit' : 'onAppendClick'
+  },
+
+  /**
+   * 日付が変更された時のイベントハンドラ
+   * @method onDateChange
+   * @private
+   */
+  onDateChange: function() {
+    var $date = this.$('.todo__append__date');
+
+    this.model.set('limitDate', $date.val());
+  },
+
+  /**
+   * 内容が変更された時のイベントハンドラ
+   * @method onContentChange
+   * @private
+   */
+  onContentChange: function() {
+    var $content = this.$('.todo__append__content');
+
+    this.model.set('content', $content.val());
   },
 
   /**
@@ -17,7 +45,34 @@ module.exports = Backboen.View.extend({
    * @private
    */
   onAppendClick: function() {
-    
+    this.listenToOnce(this.model, 'sync', function() {
+      this.collection.add(this.model);
+
+      this.refreshTodo();
+    }, this);
+
+    this.model.save({});
+  },
+
+  refreshTodo : function() {
+    var todo = new Todo();
+    this._unbindEvents();
+
+    this.model = todo;
+    this._bindEvents();
+  },
+
+  _unbindEvents : function() {
+    this.stopListening(this.model);
+  },
+
+  _bindEvents : function() {
+    this.listenTo(this.model, 'change'. this.handleChange);
+  },
+
+  handleChange : function() {
+    this.$('.todo__append__content').val(this.model.get('content'));
+    this.$('.todo__append__date').val(this.model.get('limitDate'));
   },
 
   render: function() {
